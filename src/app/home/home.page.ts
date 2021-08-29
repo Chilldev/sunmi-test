522fa014090438d75a1d2452654dba80d6b428e1
Mohammad Ali <mohammedalibis@gmail.com>
2021-08-29 19:03:56 +0200

    Testing AR text.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,7 +10,7 @@ import { SunmiInnerPrinterService } from '../services/sunmi-inner-printer.servic
 export class HomePage {
 
   constructor(private sunmi: SunmiInnerPrinterService) {}
-  async print() {
+  async printEN() {
     await this.sunmi.sunmiInnerPrinter.setAlignment(1);
     await this.sunmi.sunmiInnerPrinter.printBitmap(logo, 384, 384);
     await this.sunmi.sunmiInnerPrinter.printString("\n\n");
@@ -28,6 +28,26 @@ export class HomePage {
     await this.sunmi.sunmiInnerPrinter.printString("Powered by dor-e.com");
     await this.sunmi.sunmiInnerPrinter.printString("\n");
     await this.sunmi.sunmiInnerPrinter.printString("29/08/2020 - 02:30 PM");
+    await this.sunmi.sunmiInnerPrinter.printString("\n\n\n\n\n\n");
+  }
+  async printAR() {
+    await this.sunmi.sunmiInnerPrinter.setAlignment(1);
+    await this.sunmi.sunmiInnerPrinter.printBitmap(logo, 384, 384);
+    await this.sunmi.sunmiInnerPrinter.printString("\n\n");
+    await this.sunmi.sunmiInnerPrinter.setFontSize(100);
+    await this.sunmi.sunmiInnerPrinter.printString('١٢٣٤');
+    await this.sunmi.sunmiInnerPrinter.setFontSize(30);
+    await this.sunmi.sunmiInnerPrinter.printString("\n\n");
+    await this.sunmi.sunmiInnerPrinter.printString("قسم أ - قسم فرعي ج - خدمة أ");
+    await this.sunmi.sunmiInnerPrinter.printString("\n\n");
+    await this.sunmi.sunmiInnerPrinter.printQRCode("https://dor-e.com", 6, 3);
+    await this.sunmi.sunmiInnerPrinter.printString("\n\n");
+    await this.sunmi.sunmiInnerPrinter.printString("إمسح رمز الإستجابة السريع لتتبع دورك في الطابور");
+    await this.sunmi.sunmiInnerPrinter.printString("\n\n");
+    await this.sunmi.sunmiInnerPrinter.setFontSize(24);
+    await this.sunmi.sunmiInnerPrinter.printString("طور بواسطة dor-e.com");
+    await this.sunmi.sunmiInnerPrinter.printString("\n");
+    await this.sunmi.sunmiInnerPrinter.printString("٢٩/٠٨/٢٠٢٠ - ٠٢:٣٠ م");
     await this.sunmi.sunmiInnerPrinter.printString("\n\n\n\n\n\n");
 
   }
